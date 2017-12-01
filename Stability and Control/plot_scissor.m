4ed22e46bd91078f3e7b6c7acace28014a0b2c33
koenbeukers <koenbeukers@wlan-145-94-156-69.wlan.tudelft.nl>
2017-12-01 11:53:49 +0100

    Impoved scissor plot stability

--- Stability and Control/plot_scissor.m
+++ Stability and Control/plot_scissor.m
@@ -21,8 +21,8 @@ function [] = plot_scissor(x_ac,l_h,c,V_h_V,de_da,C_L_h,C_La_h,C_L_Ah,C_La_Ah,C_
 
 SM = 0.05; %stability margin
 acc = 0.01; %accuracy of plot
-max_Shs = 0.5; %max area ratio for plotting
-ShS = 0:acc:max_Shs; %set surface area ratio vector
+max_ShS = 1.5; %max area ratio for plotting
+ShS = 0:acc:max_ShS; %set surface area ratio vector
 
 % stability curve
 stab_x_cg = x_ac+(C_La_h/C_La_Ah)*(1-de_da)*(ShS)*(l_h/c)*(V_h_V^2)-SM;
@@ -30,80 +30,96 @@ stab_x_cg = x_ac+(C_La_h/C_La_Ah)*(1-de_da)*(ShS)*(l_h/c)*(V_h_V^2)-SM;
 %controllability curve
 cont_x_cg = x_ac-(C_m_ac/C_L_Ah)+(C_L_h/C_L_Ah)*(ShS)*(l_h/c)*(V_h_V^2);
 
-if and(((C_La_h/C_La_Ah)*(1-de_da)*(l_h/c)*(V_h_V^2))>0,(C_L_h/C_L_Ah)*(l_h/c)*(V_h_V^2)<0) %conventional config
-    %%
-    
-    figure
-    hold on
-    
-    st=area([x_ac-SM max(stab_x_cg)],[0 max_Shs]);
+x_min = min([stab_x_cg cont_x_cg]);
+x_max = max([stab_x_cg cont_x_cg]);
+
+figure
+hold on
+%% instability area plotting
+if ((C_La_h/C_La_Ah)*(1-de_da)*(l_h/c)*(V_h_V^2))<0
+    st=area([min(stab_x_cg) x_max],[max_ShS max_ShS]);
+    st.FaceAlpha = 0.2;
+    st.LineStyle = 'none';
+    st.FaceColor = 'red';
+end
+
+if (C_L_h/C_L_Ah)*(l_h/c)*(V_h_V^2)>0 % incontrollability area plotting (only this condition has to be here because of the white area's)
+    st=area([x_min max(cont_x_cg)],[max_ShS max_ShS]);
     st.FaceAlpha = 0.2;
     st.LineStyle = 'none';
     st.FaceColor = 'red';
     
-    st=area([min(cont_x_cg) x_ac-(C_m_ac/C_L_Ah)],[max_Shs 0]);
-    st.FaceAlpha = 0.2;
-    st.LineStyle = 'none';
-    st.FaceColor = 'red';
-    
-    if min(cont_x_cg)>0
-        st=area([0 min(cont_x_cg)],[max_Shs max_Shs]);
-        st.FaceAlpha = 0.2;
-        st.LineStyle = 'none';
-        st.FaceColor = 'red';
-    end
-    
-    stab = plot(stab_x_cg,ShS,'LineWidth',2,'color','b');
-    cont = plot(cont_x_cg,ShS,'LineWidth',2,'color','r');
-    
-    legend([stab,cont],'Stability','Controllability','Location','Southeast')
-    xlabel('x_{cg} / MAC')
-    ylabel('S_{h} / S')
-    hold off
-    
-else %canard config
-    figure
-    hold on
-    
-    x_min = min([stab_x_cg cont_x_cg]);
-    x_max = max([stab_x_cg cont_x_cg]);
-    
-    st=area([min(stab_x_cg) x_max],[max_Shs max_Shs]);
-    st.FaceAlpha = 0.2;
-    st.LineStyle = 'none';
-    st.FaceColor = 'red';
-    
-    st=area([min(stab_x_cg) x_ac-SM],[max_Shs 0]);
+    st=area([min(cont_x_cg) max(cont_x_cg)],[0 max_ShS]);
     st.FaceAlpha = 1;
     st.LineStyle = 'none';
     st.FaceColor = 'white';
     
-    if not(min(stab_x_cg)==x_min)
-        st=area([x_min min(stab_x_cg)],[max_Shs max_Shs]);
+    if not(x_max==max(cont_x_cg))
+        st=area([max(cont_x_cg) x_max],[max_ShS max_ShS]);
         st.FaceAlpha = 1;
         st.LineStyle = 'none';
         st.FaceColor = 'white';
     end
+end
+
+if ((C_La_h/C_La_Ah)*(1-de_da)*(l_h/c)*(V_h_V^2))<0
+    st=area([min(stab_x_cg) max(stab_x_cg)],[max_ShS 0]);
+    st.FaceAlpha = 1;
+    st.LineStyle = 'none';
+    st.FaceColor = 'white';
     
-    st=area([min(cont_x_cg) max(cont_x_cg)],[max_Shs 0]);
+    if not(x_min==min(stab_x_cg))
+        st=area([x_min min(stab_x_cg)],[max_ShS max_ShS]);
+        st.FaceAlpha = 1;
+        st.LineStyle = 'none';
+        st.FaceColor = 'white';
+    end
+end
+
+if ((C_La_h/C_La_Ah)*(1-de_da)*(l_h/c)*(V_h_V^2))==0
+    st=area([x_ac-SM x_max],[max_ShS max_ShS]);
+    st.FaceAlpha = 0.2;
+    st.LineStyle = 'none';
+    st.FaceColor = 'red';
+elseif ((C_La_h/C_La_Ah)*(1-de_da)*(l_h/c)*(V_h_V^2))>0
+    st=area([x_ac-SM max(stab_x_cg)],[0 max_ShS]);
     st.FaceAlpha = 0.2;
     st.LineStyle = 'none';
     st.FaceColor = 'red';
     
-    if not(min(cont_x_cg)==x_min)
-        st=area([x_min min(cont_x_cg)],[max_Shs max_Shs]);
+    if not(x_max==max(stab_x_cg))
+        st=area([max(stab_x_cg) x_max],[max_ShS max_ShS]);
         st.FaceAlpha = 0.2;
         st.LineStyle = 'none';
         st.FaceColor = 'red';
     end
-    
-    stab = plot(stab_x_cg,ShS,'LineWidth',2,'color','b');
-    cont = plot(cont_x_cg,ShS,'LineWidth',2,'color','r');
-    
-    legend([stab,cont],'Stability','Controllability','Location','Southeast')
-    xlabel('x_{cg} / MAC')
-    ylabel('S_{h} / S')
-    hold off
-end
 end
 
+%% incontrollability area plotting
+if (C_L_h/C_L_Ah)*(l_h/c)*(V_h_V^2)<0
+    st=area([min(cont_x_cg) max(cont_x_cg)],[max_ShS 0]);
+    st.FaceAlpha = 0.2;
+    st.LineStyle = 'none';
+    st.FaceColor = 'red';
+    
+    if not(x_min==min(cont_x_cg))
+        st=area([x_min min(cont_x_cg)],[max_ShS max_ShS]);
+        st.FaceAlpha = 0.2;
+        st.LineStyle = 'none';
+        st.FaceColor = 'red';
+    end
+elseif (C_L_h/C_L_Ah)*(l_h/c)*(V_h_V^2)==0
+    st=area([x_min min(cont_x_cg)],[max_ShS max_ShS]);
+    st.FaceAlpha = 0.2;
+    st.LineStyle = 'none';
+    st.FaceColor = 'red';
+end
+
+stab = plot(stab_x_cg,ShS,'LineWidth',2,'color','b');
+cont = plot(cont_x_cg,ShS,'LineWidth',2,'color','r');
+
+legend([stab,cont],'Stability','Controllability','Location','Southeast')
+xlabel('x_{cg} / MAC')
+ylabel('S_{h} / S')
+hold off
+end
