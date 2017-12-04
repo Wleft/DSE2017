a6ecb0e939ffdbcce70189c71ccfaf166bc53bfd
koenbeukers <koenbeukers@wlan-145-94-156-225.wlan.tudelft.nl>
2017-12-04 16:26:48 +0100

    Added start up script

---
+++ StartDSE.m
@@ -0,0 +1,4 @@
+%% project starting script
+
+% add maps to matlab path
+addpath(genpath('Aerodynamics'),genpath('Hydrodynamics'),genpath('Operations'),genpath('Propulsion and Performance'),genpath('Stability and Control'),genpath('Structures'),genpath('Other'))
