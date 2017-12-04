8df2ac37a973c0b972e700660ead541340d3d287
koenbeukers <koenbeukers@wlan-145-94-156-225.wlan.tudelft.nl>
2017-12-04 17:07:41 +0100

    Fixed GetGoogleSpreadsheet

--- StartDSE.m
+++ StartDSE.m
@@ -2,3 +2,7 @@
 
 % add maps to matlab path
 addpath(genpath('Aerodynamics'),genpath('Hydrodynamics'),genpath('Operations'),genpath('Propulsion and Performance'),genpath('Stability and Control'),genpath('Structures'),genpath('Other'))
+
+% get current values
+raw = GetGoogleSpreadsheet('1IoZBrjcV7A9TBFl9LnCycaRduCVj8CQdqR6YlVuH_FQ');
+
