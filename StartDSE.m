2b7386470d328fde3743fb643cb61045980e38f2
koenbeukers <koenbeukers@wlan-145-94-156-225.wlan.tudelft.nl>
2017-12-04 17:47:47 +0100

    Added comma to point replacement function

--- StartDSE.m
+++ StartDSE.m
@@ -6,3 +6,9 @@ addpath(genpath('Aerodynamics'),genpath('Hydrodynamics'),genpath('Operations'),g
 % get current values
 raw = GetGoogleSpreadsheet('1IoZBrjcV7A9TBFl9LnCycaRduCVj8CQdqR6YlVuH_FQ');
 
+isString    = cellfun('isclass', raw, 'char'); % change , to .
+raw(isString) = strrep(raw(isString), ',', '.');
+
+Weights.kg = str2num(char(raw(2:5,2)));
+Weights.N = str2num(char(raw(2:5,4)));
+Weights.text = raw(2:5,1);
