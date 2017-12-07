d9dbf6d77f957879a8480b74f53031fcd4627930
koenbeukers <koenbeukers@wlan-145-94-156-225.wlan.tudelft.nl>
2017-12-07 09:46:48 +0100

    Change in cruise speed requirement

--- Operations/plot_flight_envelope.m
+++ Operations/plot_flight_envelope.m
@@ -29,7 +29,7 @@ else
     
     % requirement speed ranges [m/s]
     req_V_stall = [15, 20]/ 1.94384; %knot to m/s
-    req_V_cruise = [34, 40]/ 1.94384;
+    req_V_cruise = [30, 40]/ 1.94384;
     rangereq = 1; % set to 1 to insert speed requirements range bars in plots
     
     % required load factors [g]
