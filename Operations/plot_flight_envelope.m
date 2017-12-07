dfba5735935051a0f4ad1cac1680e2acbb6b63a6
koenbeukers <koenbeukers@wlan-145-94-156-225.wlan.tudelft.nl>
2017-12-07 09:47:53 +0100

    New example input

--- Operations/plot_flight_envelope.m
+++ Operations/plot_flight_envelope.m
@@ -1,5 +1,5 @@
 function []=plot_flight_envelope(CL_max,WS,V_B,V_C,V_D,C_L_alpha,c,option)
-% example input plot_flight_envelope(1.5,700,60,75,90,5.5,1.4,1)
+% example plot_flight_envelope(1.8,110.25,15.24,16,1.25*16,5,0.8)
 % inputs are
 % CL_max; maximum lift coefficient
 % WS; Weight over surface area (wing loading)
